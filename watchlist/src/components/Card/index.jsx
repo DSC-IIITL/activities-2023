07dcb5f0cbf487f36752c1cf4fd3dc34ac16e428
ramanujangunturu <ramanujangunturu@gmail.com>
2024-02-03 20:54:39 +0530

    added basic table layout to display data

--- watchlist/src/components/Card/index.jsx
+++ watchlist/src/components/Card/index.jsx
@@ -1,5 +1,21 @@
 import "./index.css";
+import { useContext } from "react";
+import Watchlist from "../../contexts/Watchlist";
+
 
 export default function Card(props) {
-  return <div className="card">{JSON.stringify(props)}</div>;
+  const { removeFromWatchlist } = useContext(Watchlist);
+  const handleDelete = (e) => {
+    removeFromWatchlist(e.target.id)
+    console.log("removed")
+  }
+  return (
+    <div className="card">
+      <td><h3>{props.movie.Title}</h3></td>
+      <td><p>{props.movie.Year}</p></td>
+      <td><p>{props.movie.imdbRating}</p></td>
+      <td><p>{props.movie.Runtime}</p></td>
+      <td><button className="remove" id={props.movie.imdbID} onClick={handleDelete}>Delete</button></td>
+    </div>
+  );
 }
